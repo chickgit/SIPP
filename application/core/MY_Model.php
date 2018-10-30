1672d0719e05ef1f97f81c4ed846d56bbe186283
Maisur <muhammad.rahman2@gmail.com>
2018-10-30 10:17:42 +0700

    Update application/core/MY_Model.php
    new func
    untuk join dynamic

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -45,6 +45,109 @@ class MY_Model extends CI_Model {
         
         return $data;
     }
+
+    public function commonGet($options) 
+    {
+        $select = false;
+        $table = false;
+        $join = false;
+        $order = false;
+        $limit = false;
+        $offset = false;
+        $where = false;
+        $or_where = false;
+        $single = false;
+        $where_not_in = false;
+        $like = false;
+
+        extract($options);
+
+        if ($select != false)
+            $this->db->select($select);
+
+        if ($table != false)
+            $this->db->from($table);
+
+        if ($where != false)
+            $this->db->where($where);
+
+        if ($where_not_in != false) {
+            foreach ($where_not_in as $key => $value) {
+                if (count($value) > 0)
+                    $this->db->where_not_in($key, $value);
+            }
+        }
+
+        if ($like != false) {
+            $this->db->like($like);
+        }
+
+        if ($or_where != false)
+            $this->db->or_where($or_where);
+
+        if ($limit != false) {
+
+            if (!is_array($limit)) {
+                $this->db->limit($limit);
+            } else {
+                foreach ($limit as $limitval => $offset) {
+                    $this->db->limit($limitval, $offset);
+                }
+            }
+        }
+
+
+        if ($order != false) {
+
+            foreach ($order as $key => $value) {
+
+                if (is_array($value)) {
+                    foreach ($order as $orderby => $orderval) {
+                        $this->db->order_by($orderby, $orderval);
+                    }
+                } else {
+                    $this->db->order_by($key, $value);
+                }
+            }
+        }
+
+
+        if ($join != false) {
+
+            foreach ($join as $key => $value) {
+
+                if (is_array($value)) {
+
+                    if (count($value) == 3) {
+                        $this->db->join($value[0], $value[1], $value[2]);
+                    } else {
+                        foreach ($value as $key1 => $value1) {
+                            $this->db->join($key1, $value1);
+                        }
+                    }
+                } else {
+                    $this->db->join($key, $value);
+                    $this->db->where(array(
+                        $key.'.isDelete' => 0,
+                        $key.'.isShow' => 1
+                    ));
+                }
+            }
+            $this->db->where(array(
+                $table.'.isDelete' => 0,
+                $table.'.isShow' => 1
+            ));
+        }
+
+        $query = $this->db->get();
+
+        if ($single) {
+            return $query->row();
+        }
+
+
+        return $query->result();
+    }
     
     public function get_all_jadwal_perkuliahan($where = array())
     {
