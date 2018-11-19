1567d2fbb3269f544c0e0fd1e3a560cabe3df380
Maisur <muhammad.rahman2@gmail.com>
2018-11-19 17:42:52 +0700

    Update application/models/Hari_model.php
    menambahkan query isShow

--- application/models/Hari_model.php
+++ application/models/Hari_model.php
@@ -17,7 +17,8 @@ class Hari_model extends MY_Model {
     {
         $data = array(
             "nama_hari"  => $this->input->post('nama_hari'),
-            "created_by" => $this->session_username()
+            "created_by" => $this->session_username(),
+            "isShow"     => $this->input->post('isShow')
         );
         $this->db->insert('hari', $data);
         echo "OK";
@@ -29,7 +30,8 @@ class Hari_model extends MY_Model {
         $data = array(
             "nama_hari"     => $this->input->post('upd_nama_hari'),
             "modified_date" => date('Y-m-d H:i:s'),
-            "modified_by"   => $this->session_username()
+            "modified_by"   => $this->session_username(),
+            "isShow"     => $this->input->post('upd_isShow')
         );
         $this->db->where("id_hari", $this->input->post('upd_id_hari'));
         $this->db->update('hari', $data);
